149582e4fde999d522b86d7722c3a231ee367cf5
LWangllix <vanglikas.lukas@gmail.com>
2023-09-21 21:33:13 +0300

    time zone

--- services/hydroPowerPlants.service.ts
+++ services/hydroPowerPlants.service.ts
@@ -259,7 +259,7 @@ export default class hydroPowerPlantsService extends moleculer.Service {
      SELECT COUNT(*)
      FROM events e
      WHERE e.hydro_power_plant_id = hpp.id
-       AND e.time BETWEEN DATE_TRUNC('day', CURRENT_DATE) AND (DATE_TRUNC('day', CURRENT_DATE) + INTERVAL '1 day' - INTERVAL '1 second')
+       AND e.time AT TIME ZONE 'UTC' BETWEEN DATE_TRUNC('day', CURRENT_DATE) AND (DATE_TRUNC('day', CURRENT_DATE) + INTERVAL '1 day' - INTERVAL '1 second')
        AND ((e.upper_basin >= hpp.upper_basin_max OR e.upper_basin <= hpp.lower_basin_min)
          OR e.lower_basin < hpp.lower_basin_min)
     ) AS today,
@@ -267,7 +267,7 @@ export default class hydroPowerPlantsService extends moleculer.Service {
      SELECT COUNT(*)
      FROM events e
      WHERE e.hydro_power_plant_id = hpp.id
-       AND e.time BETWEEN DATE_TRUNC('day', CURRENT_DATE) - INTERVAL '1 week' AND (DATE_TRUNC('day', CURRENT_DATE) + INTERVAL '1 day' - INTERVAL '1 second')
+       AND e.time AT TIME ZONE 'UTC' BETWEEN DATE_TRUNC('day', CURRENT_DATE) - INTERVAL '1 week' AND (DATE_TRUNC('day', CURRENT_DATE) + INTERVAL '1 day' - INTERVAL '1 second')
        AND ((e.upper_basin >= hpp.upper_basin_max OR e.upper_basin <= hpp.lower_basin_min)
          OR e.lower_basin < hpp.lower_basin_min)
     ) AS week,
@@ -275,7 +275,7 @@ export default class hydroPowerPlantsService extends moleculer.Service {
      SELECT COUNT(*)
      FROM events e
      WHERE e.hydro_power_plant_id = hpp.id
-       AND e.time BETWEEN DATE_TRUNC('day', CURRENT_DATE) - INTERVAL '1 month' AND (DATE_TRUNC('day', CURRENT_DATE) + INTERVAL '1 day' - INTERVAL '1 second')
+       AND e.time AT TIME ZONE 'UTC' BETWEEN DATE_TRUNC('day', CURRENT_DATE) - INTERVAL '1 month' AND (DATE_TRUNC('day', CURRENT_DATE) + INTERVAL '1 day' - INTERVAL '1 second')
        AND ((e.upper_basin >= hpp.upper_basin_max OR e.upper_basin <= hpp.lower_basin_min)
          OR e.lower_basin < hpp.lower_basin_min)
     ) AS month,
